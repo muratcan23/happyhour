a893bb148d71ccae385d706531f41045dc913e8d
Murat Can <muratal0606@gmail.com>
2023-10-23 23:22:46 +0300

    add img

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 function App() {
   return (
     <div className="flex flex-col ">
-      <div>
-        <header className="h-12 bg-yellow-500 text-center text-3xl font-bold underline text-purple-700 ">
+      <div className="h-16 bg-yellow-500 justify-center flex flex-row space-x-4 ">
+        <img
+          className="h-16 border-2 border-rose-500"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEm8flseo1071OcB7saryOmfdjNBlPkIUPFQ&usqp=CAU"
+          alt=""
+        />
+        <header className="text-center text-3xl font-bold underline text-purple-700 ">
           Happy Hour
         </header>
       </div>
