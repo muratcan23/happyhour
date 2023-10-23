4f4df11dd3234dac3dcad7cfc85030a9f8b81300
Murat Can <muratal0606@gmail.com>
2023-10-23 23:12:52 +0300

    create project with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="flex flex-col ">
+      <div>
+        <header className="h-12 bg-yellow-500 text-center text-3xl font-bold underline text-purple-700 ">
+          Happy Hour
+        </header>
+      </div>
+      <div>01</div>
+      <div>02</div>
+      <div>03</div>
+    </div>
+  );
+}
+
+export default App;
